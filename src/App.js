d27741f783f9fd1ff96d00a16160c523f4b4a177
Louis Iaeger <louis.iaeger@gmail.com>
2021-03-07 15:53:47 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import data from './data'
+import Review from './Review.js'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const App = () => {
-  return (
-    <>
-      <div className="container">
-        <h1>Hello World</h1>
-      </div>
-    </>
-  );
-};
+  const [reviews, setReviews] = useState(data)
+  const [index, setIndex] = useState(0)
 
-export default App;
+  const nextReview = () => {
+    setIndex((index) => {
+      return checkIndex(index + 1)
+    })
+  }
+
+  const prevReview = () => {
+    setIndex((index) => {
+      return checkIndex(index - 1)
+    })
+  }
+
+  const checkIndex = (num) => {
+    if (num > reviews.length - 1) {
+      return 0
+    }
+    if (num < 0) {
+      return reviews.length - 1
+    }
+    return num
+  }
+
+  const getRandom = () => {
+    let randomNum = Math.floor(Math.random() * reviews.length)
+    if (randomNum === index) {
+      randomNum += 1
+    }
+    console.log(randomNum)
+    setIndex(checkIndex(randomNum))
+  }
+
+  return (
+    <main>
+      <div className='container'>
+        <section className='title'>
+          <h2>Our Reviews</h2>
+          <div className='underline'></div>
+        </section>
+        <section className='review'>
+          <div className='img-container'>
+            <img className='person-img' src={reviews[index].image} alt='' />
+            <div className='quote-icon'>
+              <FaQuoteRight />
+            </div>
+          </div>
+          <h4 className='author'>{reviews[index].name}</h4>
+          <p className='job'>{reviews[index].job}</p>
+          <p className='info'>{reviews[index].text}</p>
+          <div className='btn-container'>
+            <FaChevronLeft className='prev-btn' onClick={prevReview} />
+            <FaChevronRight className='next-btn' onClick={nextReview} />
+          </div>
+          <button className='random-btn' onClick={getRandom}>
+            Suprise Me
+          </button>
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Steve Jobs',
+    job: 'Visionary / Innovator ',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.clker.com%2Fcliparts%2F7%2F4%2Fc%2F4%2F1446118397610284725jobs.png',
+    text:
+      'This is the greatest thing since the iPhone. Street art kinfolk intelligentsia skateboard vice palo santo fam everyday carry banh mi hammock cray glossier master cleanse 8-bit.',
+  },
+  {
+    id: 2,
+    name: 'Justin Timberlake',
+    job: 'Musician / Actor',
+    image:
+      'https://assets.capitalxtra.com/2008/46/justin-timberlake-1227008934-view-3.jpg',
+    text:
+      'They are bringing sexy back. Direct trade hammock pug, mixtape shoreditch food truck banjo shabby chic YOLO flexitarian biodiesel cold-pressed tofu hashtag.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://avatars.githubusercontent.com/u/21149734?s=460&u=bc828dc441972956937d361b0dfc833141ff798a&v=4',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+]
+
+export default reviews
